2fdfc91215c1031a448ddc202b9d45ae580c0fa1
Nane <nane.kds@fatoscorp.com>
2024-07-03 18:42:45 +0900

    auth folder 생성

--- lib/auth_server.g.dart
+++ lib/auth_server.g.dart
@@ -13,6 +13,24 @@ Router _$AuthServerRouter(AuthServer service) {
     r'/',
     service.scmApiServer,
   );
+  router.mount(
+    r'/auth',
+    service._auth.call,
+  );
+  router.all(
+    r'/<ignored|.*>',
+    service._notFound,
+  );
+  return router;
+}
+
+Router _$AuthRouter(Auth service) {
+  final router = Router();
+  router.add(
+    'GET',
+    r'/',
+    service.auth,
+  );
   router.all(
     r'/<ignored|.*>',
     service._notFound,
